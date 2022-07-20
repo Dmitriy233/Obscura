57fb4bad4b5e1bbf98292028eba8d18d377f2891
Dmitriy233 <109022907+Dmitriy233@users.noreply.github.com>
2022-07-20 11:31:30 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,33 @@
+const portfolio = $(".portfolio__list").bxSlider({
+    pager: false,
+    controls: false,
+    infiniteLoop: true,
+    touchEnabled: true,
+    hideControlOnEnd: false,
+});
+
+$(".button--portfolio-prev").click((e) => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+});
+$(".button--portfolio-next").click((e) => {
+    e.preventDefault();
+    slider.goToNextSlide();
+});
+
+const slider = $(".feedback__list").bxSlider({
+    pager: false,
+    controls: false,
+    infiniteLoop: true,
+    touchEnabled: true,
+    hideControlOnEnd: false,
+});
+$(".button--prev").click((e) => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+});
+
+$(".button--next").click((e) => {
+    e.preventDefault();
+    slider.goToNextSlide();
+});
